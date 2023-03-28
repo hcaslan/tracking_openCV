665335569d985825d2cdb02eed15d9743fc9abd4
hcasl <hcasl@DESKTOP-D9LM80P>
2023-03-28 12:53:50 +0300

    Add project files.

---
+++ tracking_openCV/tracking_openCV.cpp
@@ -0,0 +1,43 @@
+﻿#include "opencv2\opencv.hpp"
+#include "opencv2\highgui.hpp"
+#include "opencv2\tracking.hpp"
+
+int main() {
+
+	cv::VideoCapture video(0);
+	if (!video.isOpened())
+	{
+		return -1;
+	}
+
+	cv::Mat frame;
+
+	// Tracker 
+	cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
+	video.read(frame);
+	cv::Rect trackingBox = cv::selectROI(frame, false);
+	tracker->init(frame, trackingBox);
+
+
+	while (video.read(frame)) {
+
+		if (tracker->update(frame, trackingBox)) 
+		{
+			cv::rectangle(frame, trackingBox, cv::Scalar(255, 0, 0), 2, 8);
+		}
+
+		cv::imshow("camera", frame);
+
+		if (cv::waitKey(25) >= 0) 
+		{
+			break;
+		}
+	} 
+
+	//clear
+	video.release();
+	cv::destroyAllWindows();
+
+	return 0;
+
+}
